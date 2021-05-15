7cfde3683d79f8f1f2efe7f9d1170985d7808f08
virendra <patelvirendra62@gmail.com>
2021-05-15 15:39:34 +0530

    home

--- traveler_notes/notes/urls.py
+++ traveler_notes/notes/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, SignupView, LoginView
 
 
 urlpatterns = [
-    path('', home, name='hompage')
+    path('', home, name='hompage'),
+    path('signup', SignupView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login')
 ]
--- traveler_notes/notes/views.py
+++ traveler_notes/notes/views.py
@@ -1,7 +1,37 @@
+from django.forms.forms import Form
+from django.http import request
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.generic import FormView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
-def home(request ):
-    return HttpResponse("Home Page")
+
+def home(request):
+    return HttpResponse(f"Hello {request.user}")
+
+
+class SignupView(FormView):
+    form_class = UserCreationForm
+    template_name = 'authforms/signup.html'
+    success_url = 'login'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'authforms/login.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        # form.save()
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        login(request=self.request, user=user)
+        return super().form_valid(form)

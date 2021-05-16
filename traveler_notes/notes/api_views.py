7f38337637ad9cb2c861dca751c9a62010350076
virendra <patelvirendra62@gmail.com>
2021-05-16 16:30:37 +0530

    marker adding

--- traveler_notes/notes/api_views.py
+++ traveler_notes/notes/api_views.py
@@ -1,11 +1,13 @@
+from rest_framework import serializers
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from .models import NoteSerializer, Note
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-class CreateNote(APIView):
+class NoteApiView(APIView):
     authentication_classes = [SessionAuthentication]
     permission_classes = [IsAuthenticated]
 
@@ -18,4 +20,9 @@ class CreateNote(APIView):
         note = Note(title=title, comment=comment, lat=lat, lng=lng, user=user)
         note.save()
         serializer = NoteSerializer(note)
+        return Response(serializer.data)
+
+    def get(self, request):
+        notes = Note.objects.filter(user=request.user)
+        serializer = NoteSerializer(notes, many=True)
         return Response(serializer.data)
--- traveler_notes/notes/urls.py
+++ traveler_notes/notes/urls.py
@@ -2,12 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home, SignupView, LoginView
-from .api_views import CreateNote
+from .api_views import NoteApiView
 
 
 urlpatterns = [
     path('', home, name='hompage'),
     path('signup', SignupView.as_view(), name='signup'),
     path('login', LoginView.as_view(), name='login'),
-    path('notes/create', CreateNote.as_view(), name='create-note'),
+    path('api/notes', NoteApiView.as_view(), name='create-note'),
 ]
